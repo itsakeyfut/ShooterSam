960a8923ea5119972e481027d65ed5bc3665e72d
itsakeyfut <sicsempertyrannis98@gmail.com>
2026-02-10 00:38:27 +0900

    feat: ライフル銃をプレイヤーの手元に配置

--- Source/ShooterSam/ShooterSamCharacter.cpp
+++ Source/ShooterSam/ShooterSamCharacter.cpp
@@ -54,10 +54,13 @@ void AShooterSamCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
+	GetMesh()->HideBoneByName("weapon_r", EPhysBodyOp::PBO_None);
+
 	Gun = GetWorld()->SpawnActor<AGun>(GunClass);
 	if (Gun)
 	{
 		Gun->SetOwner(this);
+		Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, TEXT("WeaponSocket"));
 	}
 }
 
